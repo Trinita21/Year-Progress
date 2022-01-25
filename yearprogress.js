82f1ed4e397fcd9d9c6362bce6fe329d99372bef
trinita21 <trinita20221@iiitd.ac.in>
2022-01-25 16:17:57 +0530

    Calculation completed Progress bar added

--- yearprogress.js
+++ yearprogress.js
@@ -1,4 +1,10 @@
-let today_date = new Date()
-let present_year = new Date(new Date().getFullYear(),0,1)
+let today_date = new Date().getTime();
+let present_year = new Date(new Date().getFullYear(), 0, 1).getTime();
+let next_year = new Date(new Date().getFullYear() + 1, 0, 1).getTime();
 
-console.log(present_year)
+function GetProgress(today_date, present_year, next_year) {
+  let progress =
+    ((today_date - present_year) / (next_year - present_year)) * 100;
+  return progress;
+}
+console.log(GetProgress(today_date, present_year, next_year));
