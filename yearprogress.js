08a13f4479d541b9203195195858250473b4fee3
trinita21 <trinita20221@iiitd.ac.in>
2022-01-25 17:54:08 +0530

    Styling completed

--- yearprogress.js
+++ yearprogress.js
@@ -5,8 +5,11 @@ let next_year = new Date(new Date().getFullYear() + 1, 0, 1).getTime();
 function GetProgress(today_date, present_year, next_year) {
   let progress =
     ((today_date - present_year) / (next_year - present_year)) * 100;
-  return progress;
+  return Math.round(progress);
 }
 let year_progress = GetProgress(today_date, present_year, next_year);
 
-document.getElementById("progress_bar").setAttribute("value", year_progress);
+document.getElementById("bar").style = "width:" + year_progress + "%";
+
+document.getElementById("result").innerText =
+  year_progress + "% of year has passed by and still you are procrastinating";
